88612cd715ea8fc60c8e87083c5f64998563d5af
Gloaty <95916698+Gloaty@users.noreply.github.com>
2022-01-27 10:33:36 +0000

    Add files via upload

---
+++ main (40).py
@@ -0,0 +1,8 @@
+#this program will be an all-in-one calculator
+
+#imports
+import math
+
+#welcome
+print("Welcome to Calculator Ωmega!")
+operation_choice = input("Please input required operator: (+, -, *, /, %, **) ")
