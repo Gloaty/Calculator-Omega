23ab5ac9cf8ba474a18a0ce9586fd7e81bf309a1
Gloaty <95916698+Gloaty@users.noreply.github.com>
2022-09-17 15:31:18 +0100

    Delete calculator_omega1.py

--- calculator_omega1.py
+++
@@ -1,8 +0,0 @@
-#this program will be an all-in-one calculator
-
-#imports
-import math
-
-#welcome
-print("Welcome to Calculator Ωmega!")
-operation_choice = input("Please input required operator: (+, -, *, /, %, **) ")
